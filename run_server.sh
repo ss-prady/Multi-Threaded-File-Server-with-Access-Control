1c9bab5e6ce15535d7c3dbbfc28fb6add737b3f1
Pradyuman Singh Shekhawat <147436477+ss-prady@users.noreply.github.com>
2025-04-27 05:31:15 +0530

    Update run_server.sh

--- run_server.sh
+++ run_server.sh
@@ -1,25 +1,3 @@
-# #!/bin/bash
-
-# clear
-# echo "=============================================="
-# echo "        FileServer Setup & Launch Script      "
-# echo "=============================================="
-
-# echo "[*] Creating build directory..."
-# mkdir -p build
-# cd build
-
-# echo "[*] Running CMake..."
-# cmake .. || { echo "[-] CMake configuration failed!"; exit 1; }
-
-# echo "[*] Building project..."
-# make || { echo "[-] Build failed!"; exit 1; }
-
-# echo "----------------------------------------------"
-# echo "[*] Launching File Server..."
-# cd server 
-# ./file_server
-
 #!/bin/bash
 
 clear
